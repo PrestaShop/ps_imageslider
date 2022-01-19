cb63a230270fbd222cc811d8cb21a89240d21925
Mahmoud Parvazi <85633460+mparvazi@users.noreply.github.com>
2022-01-19 13:33:13 +0330

    Update ps_imageslider.php
    Co-authored-by: saulaski <97030953+saulaski@users.noreply.github.com>

--- ps_imageslider.php
+++ ps_imageslider.php
@@ -151,7 +151,8 @@ class Ps_ImageSlider extends Module implements WidgetInterface
                 $slide->url[$language['id_lang']] = 'http://www.prestashop.com/?utm_source=back-office&utm_medium=v17_homeslider'
                     . '&utm_campaign=back-office-' . Tools::strtoupper($this->context->language->iso_code)
                     . '&utm_content=' . (defined('_PS_HOST_MODE_') ? 'ondemand' : 'download');
-                $slide->image[$language['id_lang']] = 'sample-' . $i . ($language['is_rtl'] ? '-rtl' : '') . '.jpg';
+                $rtlSuffix = $language['is_rtl'] ? '-rtl' : '';
+                $slide->image[$language['id_lang']] = "sample-{$i}{$rtlSuffix}.jpg";
             }
             $slide->add();
         }
