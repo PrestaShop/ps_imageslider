f01e0559b959b4d2894bee38e5abb5f030fdad3e
mattgoud <matthias.goudjil@prestashop.com>
2026-09-08 21:30:14 +0200

    Bump module version to 4.0.0
    Raising the minimum PrestaShop version from 1.7.x to 8.2.0 is a breaking
    change, so the next release is a major. Also updates the compatibility
    line of the README, which still announced 8.1.0.

--- ps_imageslider.php
+++ ps_imageslider.php
@@ -52,7 +52,7 @@ class Ps_ImageSlider extends Module implements WidgetInterface
     {
         $this->name = 'ps_imageslider';
         $this->tab = 'front_office_features';
-        $this->version = '3.2.2';
+        $this->version = '4.0.0';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
         $this->secure_key = Tools::hash($this->name);
